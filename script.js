ae443454c31162b37e2ac54cdb2c356b19102e91
Sudarshan Suryaprakash <sudarshan@Sudarshans-MacBook-Air.local>
2020-07-21 01:09:21 +0100

    setting local storage

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const container = document.querySelector('.container');
-const seats = document.querySelector('.row .seat:not(.occupied)');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 let count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
+const setMovieData = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
 
 const updateSelectedCount = () => {
-  const selectedSeatsCount = document.querySelectorAll('.row .seat.selected')
-    .length;
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+  const seatsIndex = [...selectedSeats].map((seat) => {
+    return [...seats].indexOf(seat);
+  });
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 };
 
 container.addEventListener('click', (e) => {
@@ -23,4 +31,5 @@ container.addEventListener('click', (e) => {
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
   updateSelectedCount();
+  setMovieData(e.target.selectedIndex, e.target.value);
 });
